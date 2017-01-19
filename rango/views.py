edc46e1db645a1b73e41a25ef40929504fa43c27
Jaskaran Bansal <2185633b@student.gla.ac.uk>
2017-01-19 19:04:19 +0000

    Update2

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context = context_dict)
     return HttpResponse("Rango says hey there partner!")
 
 def about(request):
