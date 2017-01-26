7e79e5ddcef6fcaa99b7bc435eb4f606f96d0776
Jaskaran Bansal <2185633b@student.gla.ac.uk>
2017-01-26 21:26:34 +0000

    Finished Chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
+#class added to customise the admin interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
 
